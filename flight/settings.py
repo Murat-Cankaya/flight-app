405e9a0e8bb7a9ef97d6737721ec0b48f492356d
Murat Cankaya <muratcankaya@Murat-MacBook-Pro.local>
2025-08-11 14:13:25 +0300

    login logout register halloldu

--- flight/settings.py
+++ flight/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'account',
     'uçuşlar',
 ]
 
@@ -57,7 +57,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
          'DIRS': [os.path.join(BASE_DIR, 'uçuslar', 'templates')],
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 
 # Default primary key field type
